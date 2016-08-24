e2b9bf89541d442d6ab1acde4478222f7d8f1ac0
MarkeJave <308865427@qq.com>
2016-08-24 16:37:05 +0800

    no message

---
+++ MantleExtensionDemo/MantleExtensionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MantleExtensionDemo
+//
+//  Created by dihong on 16/8/24.
+//  Copyright © 2016年 Marke Jave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MantleExtensionDemo/MantleExtensionDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MantleExtensionDemo
+//
+//  Created by dihong on 16/8/24.
+//  Copyright © 2016年 Marke Jave. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
